d438a0ef8a27baf2ccae3150a33292065381937c
Artem <artem.maksakov@hneu.net>
2022-10-20 03:04:17 +0300

    node-HW3 added tasks 3-5

--- test/main.test.js
+++ test/main.test.js
@@ -1,6 +1,72 @@
 import * as assert from "assert";
 import Calculator from "../main.js";
 
+function doTest(braces, expected) {
+  const actual = checkBraces(braces);
+  assert.strictEqual(actual, expected, `for braces:\n"${braces}"\n`);
+}
+function checkBraces(braces) {
+  if (braces.length % 2 !== 0) return false;
+  const openBraces = ["(", "{", "["];
+  let bracketArr = [];
+  let result = true;
+  braces.split("").forEach((brecket) => {
+    if (openBraces.includes(brecket)) bracketArr.push(brecket);
+    else {
+      const lastBracket = bracketArr.pop();
+      if (brecket !== closeBracket(lastBracket)) {
+        result = false;
+        return;
+      }
+    }
+  });
+  return result ? bracketArr.length === 0 : result;
+}
+function closeBracket(bracket){
+  switch(bracket){
+    case '(':
+      return ')';
+    case '{':
+      return '}';
+    case '[':
+      return ']';
+  }
+}
+const symbolQuantity = 4;
+
+function maskify(cc) {
+  const arrCC = cc.split("");
+
+  return arrCC.length > symbolQuantity
+    ? arrCC
+        .map((el, i, arr) =>
+          arr.length - symbolQuantity > i ? (el = "#") : el
+        )
+        .join("")
+    : cc;
+}
+function randomString() {
+  return Math.random()
+    .toString(36)
+    .substring(2, 5 + Math.random() * 10);
+}
+function getMask(original) {
+  let masked = [];
+  for (let i = 0; i < original.length - 4; i++) masked.push("#");
+  for (let i = original.length - 4; i < original.length; i++)
+    masked.push(original[i]);
+  return masked.join("");
+}
+
+function narcissistic(value) {
+  const nums = Array.from(String(value), Number);
+  const narcissisticVal = nums.reduce(
+    (accumulator, currentValue, index, array) =>
+      accumulator + Math.pow(+currentValue, array.length),
+    0
+  );
+  return narcissisticVal === value;
+}
 describe("Calculator testing", function () {
   let calc = new Calculator();
   it("Checking sum function with mantissa.", function () {
@@ -42,5 +108,48 @@ describe("Calculator testing", function () {
       "Operator type is invalid"
     );
   });
+});
+describe("Maskify function test", function () {
+  for (let i = 0; i < 200; i++) {
+    let temp = randomString() + randomString();
+    let temp2 = getMask(temp);
+    it("Checking " + temp, function () {
+      assert.strictEqual(maskify(temp), temp2);
+    });
+  }
+});
+describe("Narcissistic function test", function () {
+  let size = Math.floor(Math.random() * 10) + 5;
+  for (let i = 0; i < size; i++) {
+    let value = Math.floor(Math.random() * 42000) + 9475;
 
+    it("Checking " + value, function () {
+      assert.strictEqual(
+        narcissistic(value),
+        false,
+        value + "is not narcissistic"
+      );
+    });
+  }
+});
+
+describe("Tests suite", function () {
+  it("sample tests", function () {
+    doTest("()))", false);
+    doTest("()", true);
+    doTest("[]", true);
+    doTest("{}", true);
+    doTest("(){}[]", true);
+    doTest("([{}])", true);
+    doTest("(}", false);
+    doTest("[(])", false);
+    doTest("({})[({})]", true);
+    doTest("(})", false);
+    doTest("(({{[[]]}}))", true);
+    doTest("{}({})[]", true);
+    doTest(")(}{][", false);
+    doTest("())({}}{()][][", false);
+    doTest("(((({{", false);
+    doTest("}}]]))}])", false);
+  });
 });
