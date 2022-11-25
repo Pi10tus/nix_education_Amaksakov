fe0360ba27c76550e6875e59233fd1db25fe16a6
Artem <artem.maksakov@hneu.net>
2022-11-26 01:28:58 +0200

    node-module-update8

--- app.js
+++ app.js
@@ -10,19 +10,19 @@ const password = encodeURIComponent("fZ86JkEjrjJiYz5A");
 const app = express();
 app.use(express.json());
 app.use(express.static(__dirname));
-app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname, "/index.html"));
-});
-app.use("/auth", authRouter);
-const start = async () => {
-  try {
-    await mongoose.connect(
-      `mongodb+srv://${username}:${password}@cluster0.cekqq4q.mongodb.net/Maksakov_edu_nix?retryWrites=true&w=majority`
-    );
+// app.get("/", function (req, res) {
+//   res.sendFile(path.join(__dirname, "/index.html"));
+// });
+// app.use("/auth", authRouter);
+// const start = async () => {
+//   try {
+//     await mongoose.connect(
+//       `mongodb+srv://${username}:${password}@cluster0.cekqq4q.mongodb.net/Maksakov_edu_nix?retryWrites=true&w=majority`
+//     );
 
-    app.listen(PORT, () =>
-      console.log(`Server is running on http://${HOST}:${PORT}`)
-    );
-  } catch (e) {}
-};
+//     app.listen(PORT, () =>
+//       console.log(`Server is running on http://${HOST}:${PORT}`)
+//     );
+//   } catch (e) {}
+// };
 start();
