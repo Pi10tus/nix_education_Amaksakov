d961ee42e58fe301259f3bcf921ff8f799d56c03
Artem <artem.maksakov@hneu.net>
2022-11-26 15:49:39 +0200

    node-module-update13

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const cors = require("cors");
 const express = require("express");
 const { default: mongoose } = require("mongoose");
 
@@ -8,19 +9,21 @@ const HOST = "localhost";
 const username = encodeURIComponent("Pilotus42");
 const password = encodeURIComponent("fZ86JkEjrjJiYz5A");
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(express.static(__dirname));
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
 app.use("/auth", authRouter);
+
 const start = async () => {
   try {
     await mongoose.connect(
       `mongodb+srv://${username}:${password}@cluster0.cekqq4q.mongodb.net/Maksakov_edu_nix?retryWrites=true&w=majority`
     );
 
-    app.listen((process.env.PORT || PORT), () =>
+    app.listen(process.env.PORT || PORT, () =>
       console.log(`Server is running on http://${HOST}:${PORT}`)
     );
   } catch (e) {}
