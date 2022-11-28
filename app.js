e6e13c4d3f8fa4318f7d5e676ed8391fe617b9c3
Artem <artem.maksakov@hneu.net>
2022-11-28 18:13:45 +0200

    node-module-update16

--- app.js
+++ app.js
@@ -15,13 +15,13 @@ app.use(express.static(__dirname));
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
-// app.use("/auth", authRouter);
+app.use("/auth", authRouter);
 
 const start = async () => {
   try {
-    await mongoose.connect(
-      `mongodb+srv://${username}:${password}@cluster0.cekqq4q.mongodb.net/Maksakov_edu_nix?retryWrites=true&w=majority`
-    );
+    // await mongoose.connect(
+    //   `mongodb+srv://${username}:${password}@cluster0.cekqq4q.mongodb.net/Maksakov_edu_nix?retryWrites=true&w=majority`
+    // );
 
     app.listen(process.env.PORT || PORT, () =>
       console.log(`Server is running on http://${HOST}:${PORT}`)
